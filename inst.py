8372d488d1c0d265bc2f01640a6a68fb494ed1ea
raja.asim.fayyaz <raja.asim.fayyaz@gmail.com>
2021-08-04 16:11:46 +0500

    added average per field in inst_info API

--- inst.py
+++ inst.py
@@ -144,16 +144,22 @@ def get_inst_info(inst):
     cols=list(sub_df)
     data['sic']={}
     data['sic']['minor']={}
-    
+    data['sic']['minor_global_average']={}
+
     for col in cols:
         data['sic']['minor'][get_sic_name_from_column(col)]=sub_df[col].values[0]
-        
+        # add average per field as well
+        data['sic']['minor_global_average'][get_sic_name_from_column(col)]=get_inst_stats_for_field(get_sic_name_from_column(col))[0]
+    
     sub_df=sic[major_cols]
     sub_df=sub_df.replace(0,np.nan).dropna(axis=1,how="all")# removing 0s
     cols=list(sub_df)
     data['sic']['major']={}
+    data['sic']['major_global_average']={}
     for col in cols:
         data['sic']['major'][get_sic_name_from_column(col)]=sub_df[col].values[0]
+        # add average per field as well
+        data['sic']['major_global_average'][get_sic_name_from_column(col)]=get_inst_stats_for_field(get_sic_name_from_column(col))[0]
     
     df1=sci_per_country.get_scientific_scores_major_and_minor_fields_col_dic() # populate sci data
     
@@ -168,15 +174,21 @@ def get_inst_info(inst):
     cols=list(sub_df)
     data['sci']={}
     data['sci']['minor']={}
-    
+    data['sic']['minor_global_average']={}
     for col in cols:
-        data['sci']['minor'][get_sic_name_from_column(col)]=sub_df[col].values[0]
-        
+        data['sci']['minor'][get_sci_name_from_column(col)]=sub_df[col].values[0]
+        # add average per field as well
+        data['sic']['minor_global_average'][get_sci_name_from_column(col)]=get_inst_stats_for_field(get_sci_name_from_column(col))[0]
+    
     sub_df=sic[major_cols]
     sub_df=sub_df.replace(0,np.nan).dropna(axis=1,how="all")# removing 0s
     cols=list(sub_df)
     data['sci']['major']={}
+    data['sic']['major_global_average']={}
     for col in cols:
         data['sci']['major'][get_sic_name_from_column(col)]=sub_df[col].values[0]
+        # add average per field as well
+        data['sic']['major_global_average'][get_sci_name_from_column(col)]=get_inst_stats_for_field(get_sci_name_from_column(col))[0]
+    
     return data
 

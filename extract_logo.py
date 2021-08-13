dcf330dc0a886d2045094e294c9511402c59cb8a
raja.asim.fayyaz <raja.asim.fayyaz@gmail.com>
2021-08-13 21:25:31 +0500

    added extract logo new

--- extract_logo.py
+++ extract_logo.py
@@ -30,7 +30,7 @@ db_connection_str = db_string
 db_connection = create_engine(db_connection_str,connect_args= connect_args)
 
 def get_company_url(): # to get url and name of companies
-    sql="SELECT name,homepage_url FROM acp_companies"
+    sql="SELECT name,homepage_url FROM new_companies"
     df = pd.read_sql(sql, db_connection)
     return df
 
@@ -40,9 +40,6 @@ print("full df shape: ", df.shape)
 
 print("unique names: ", len(df['name'].unique()))
 print("unique urls: ", len(df['homepage_url'].unique()))
-print("----")
-print("unique names: ", len(set(df['name'].values)))
-print("unique urls: ", len(set(df['homepage_url'].values)))
 
 data={} # to store csv of images and companies
 data['name']=[]
@@ -56,45 +53,28 @@ except:
 
 base_url="https://logo.clearbit.com/" # clearbit used to download logo
 
-names_done=[]
-
-non_names=0
-
 with tqdm(total=len(df)) as pbar:
     for i,row in df.iterrows():
         url=row['homepage_url'].replace('http://','')
         url=url.replace('https://','') # clean the url
         new_url=base_url+url
-        if row['name']=="" or row['name']==None: # if no name given
-            non_names+=1
-        if row['name'] not in names_done:
-            try:
-                r = requests.get(url = new_url, stream=True) # get logo
-                if r.status_code==200: # if logo found
-                    with open('logo_images/'+row['name']+'.png', 'wb') as out_file:
-                        shutil.copyfileobj(r.raw, out_file)
-                    data['name'].append(row['name'])
-                    data['homepage_url'].append(row['homepage_url'])
+        try:
+            r = requests.get(url = new_url, stream=True) # get logo
+            if r.status_code==200: # if logo found
+                with open('logo_images/'+row['name']+'.png', 'wb') as out_file:
+                    shutil.copyfileobj(r.raw, out_file)
+                data['name'].append(row['name'])
+                data['homepage_url'].append(row['homepage_url'])
 
-                    with open('logo_images/'+row['name']+'.png', "rb") as image_file:
-                        encoded_string = base64.b64encode(image_file.read()) # get base64 from image
-                    data['base64_img'].append(encoded_string)
-                    names_done.append(row['name']) # to keep track of all the names
-            except Exception as e:
-                print("EXC: ", e)
-                pass
+                with open('logo_images/'+row['name']+'.png', "rb") as image_file:
+                    encoded_string = base64.b64encode(image_file.read()) # get base64 from image
+                data['base64_img'].append(encoded_string)
+        except Exception as e:
+            print("EXC: ", e)
+            pass
         pbar.update(1)
 
 df1=pd.DataFrame(data)
 print("df1: ", df1.shape)
 
-df1.to_csv("company_logos1.csv")
-
-print("names found: ", names_done)
-
-import pickle
-
-with open('unique_company_names.pickle', 'wb') as handle:
-    pickle.dump(names_done, handle)
-
-print("null names: ", non_names)
+df1.to_csv("company_logos.csv")
